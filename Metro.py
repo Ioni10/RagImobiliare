fc36bd204de7dfdcf10856cca6a92bf149609861
ManduIon <manduioncostinel@gmail.com>
2025-11-21 19:43:19 +0200

    "Normalizare cartiere + park distanta"

--- Metro.py
+++ Metro.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 from geopy.distance import geodesic
+import geopandas as gpd
 #Citim
 with open("metrodata.json", "r", encoding="utf-8") as f:
     data = json.load(f)
@@ -23,8 +24,28 @@ def timp_metro(lat,lon):
     timp_min = (metro_aproape / km_h) * 60
     return round(timp_min, 1)
 
+
+
 data['dist_to_metro_min'] = data.apply(lambda row: timp_metro(row['lat'], row['lon']), axis=1)
 
-data.to_csv("olx_bucuresti_imobiliare_metro.csv", index=False)
+#data.to_csv("olx_bucuresti_imobiliare_metro.csv", index=False)
 
+parc= gpd.read_file("parc.geojson")
 
+parc['lon'] = parc.geometry.centroid.x
+parc['lat'] = parc.geometry.centroid.y
+
+#parc[['name', 'lat', 'lon']].to_csv("parcuri.csv", index=False)
+parc_data = pd.read_csv("parcuri.csv")
+
+def timp_park(lat,lon):
+    locatie = (lat, lon)
+    distante = parc_data.apply(lambda row: geodesic(locatie, (row['lat'], row['lon'])).km, axis=1)
+    parc_aproape = distante.min()
+    km_h = 12
+    timp_min = (parc_aproape / km_h) * 60
+    return round(timp_min, 1)
+
+data['dist_to_park_min'] = data.apply(lambda row: timp_park(row['lat'], row['lon']), axis=1)
+
+#data.to_csv("Bucurestiv2.csv", index=False)
--- Normalizare.py
+++ Normalizare.py
@@ -1,9 +1,32 @@
 import pandas as pd
 
-df = pd.read_csv("olx_bucuresti_imobiliare_enhanced.csv")
+# df = pd.read_csv("Bucurestiv2.csv")
+# df['parking'] = df['parking'].map({"yes": 1, "unknown": 0})
+# df['parking'] = df['parking'].astype(int)
+#
+# df.to_csv("Bucurestiv3.csv", index=False)
+
+df = pd.read_csv("Bucurestiv3.csv")
 
 
-df['parking'] = df['parking'].map({"yes": 1, "unknown": 0})
-df['parking'] = df['parking'].astype(int)
+df.loc[df['region'] == 'Rahova', 'neighborhood_real'] = 'Sector 5'
+df.loc[df['region'] == 'Brâncuși', 'neighborhood_real'] = 'Sector 6'
+df.loc[df['region'] == 'Dămăroaia', 'neighborhood_real'] = 'Sector 6'
+df.loc[df['region'] == 'Drumul Taberei', 'neighborhood_real'] = 'Sector 6'
+df.loc[df['region'] == 'Centrul Civic', 'neighborhood_real'] = 'Sector 3'
+df.loc[df['region'] == 'Gramont', 'neighborhood_real'] = 'Sector 4'
+df.loc[df['region'] == 'Bucureștii Noi', 'neighborhood_real'] = 'Sector 1'
+df.loc[df['region'] == 'Grozăvești', 'neighborhood_real'] = 'Sector 6'
+df.loc[df['region'] == 'Ghencea', 'neighborhood_real'] = 'Sector 6'
+df.loc[df['region'] == 'Progresul', 'neighborhood_real'] = 'Sector 4'
+df.loc[df['region'] == 'Titan - Balta Albă', 'neighborhood_real'] = 'Sector 3'
+df.loc[df['region'] == 'Traian', 'neighborhood_real'] = 'Sector 2'
+df.loc[df['region'] == 'Colentina', 'neighborhood_real'] = 'Sector 2'
+df.loc[df['region'] == 'Doamna Ghica', 'neighborhood_real'] = 'Sector 2'
+df.loc[df['region'] == 'Tineretului', 'neighborhood_real'] = 'Sector 4'
+df.loc[df['region'] == 'Grivița', 'neighborhood_real'] = 'Sector 1'
+df.loc[df['region'] == 'Fundeni', 'neighborhood_real'] = 'Sector 2'
 
-df.to_csv("Bucuresti.csv", index=False)
+print(df['neighborhood_real'].head(20))
+
+df.to_csv("bbb.csv", index=False)
